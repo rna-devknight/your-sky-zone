38c3b8658131d5858a253d6133f5710356c3c107
rna-devknight <ricardo.nunesalcobia@knights.ucf.edu>
2021-11-14 05:36:42 -0500

    Integrating opensky API

--- midway.py
+++ midway.py
@@ -1,11 +1,33 @@
-# import time
-# import datetime from time
-# from datetime import time
-# import serial from datetime
-# from datetime import serial
+from opensky_api import OpenSkyApi
 import serial
-# import gmtime
-# import strftime
+
+localCallsigns = ["Hello"]
+
+# BB = Bounding Box
+# Small BB for day
+# If taking bounding box from:https://boundingbox.klokantech.com/
+# Order has to be changed
+# 3 1 4 2
+# -81.6417, 28.767, -81.2132, 29.0027
+# 28.767, 29.0027, -81.6417, -81.2132
+
+# Large BB for night
+# -82.7711,28.4265,-81.0792,29.9228
+# 28.4265, 29.9228, -82.7711, -81.0792
+api = OpenSkyApi()
+# BB for North Central Florida
+states = api.get_states(bbox=(28.4265, 29.9228, -82.7711, -81.0792))
+
+for s in states.states:
+    temp = s.callsign
+    # print(temp)
+    localCallsigns.append(temp)
+
+# print(localCallsigns)
+
+for stuff in localCallsigns:
+    print(stuff)
+
 
 # Open serial port
 ser = serial.Serial()
@@ -18,7 +40,7 @@ ser.bytesize = serial.EIGHTBITS
 ser.timeout = 1
 
 # Check which port was used
-print(ser.name)
+# print(ser.name)
 
 # Makes sure to open the port
 if(ser.isOpen() == False):
@@ -45,37 +67,37 @@ exit = False
 # print(whole.upper())
 
 
-while incomplete:
-    # print(str(count) + ": " + "Sending " + inputString[count] + " to MC")
-    hero = inputString[count]
-    hero = bytes(hero, encoding='ascii')
+# while incomplete:
+#     # print(str(count) + ": " + "Sending " + inputString[count] + " to MC")
+#     hero = inputString[count]
+#     hero = bytes(hero, encoding='ascii')
 
-    ser.write(hero)
+#     ser.write(hero)
 
-    for x in range(200):
-        mcReader = ser.read(count + 1).decode('ascii')
-        # If it contains a digit or number
-        if mcReader.isalnum:
-            # print("Searching for: " + mcReader)
+#     for x in range(200):
+#         mcReader = ser.read(count + 1).decode('ascii')
+#         # If it contains a digit or number
+#         if mcReader.isalnum:
+#             # print("Searching for: " + mcReader)
 
-            if mcReader in whole:
-                # print("Within: " + mcReader)
-                count += 1
-                tracker += mcReader
-                if count == 7:
-                    incomplete = False
-                    exit = True
+#             if mcReader in whole:
+#                 # print("Within: " + mcReader)
+#                 count += 1
+#                 tracker += mcReader
+#                 if count == 7:
+#                     incomplete = False
+#                     exit = True
 
-                break
+#                 break
 
-    if exit:
-        incomplete = False
-        break
+#     if exit:
+#         incomplete = False
+#         break
 
-hero = inputString[len(inputString) - 1]
-hero = bytes(hero, encoding='ascii')
-for shutup in range(4):
-    ser.write(hero)
+# hero = inputString[len(inputString) - 1]
+# hero = bytes(hero, encoding='ascii')
+# for shutup in range(4):
+#     ser.write(hero)
 
 
 # print("Obtained input:")
